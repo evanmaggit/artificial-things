4141cad4ea0d504d4974bb1000793a25c5472229
S <christopher.weston@wsu.edu>
2026-04-17 03:38:28 +0000

    Added hide-and-reveal function, testing phase 1

--- themes/naptime-nuisance/scripts.js
+++ themes/naptime-nuisance/scripts.js
@@ -15,4 +15,33 @@ export function initializePage() {
     // call main function that outputs message to console and update page title
     confirmTheme("Naptime Nuisance");
 
+    const revealItems = document.querySelectorAll("main p, main aside, footer");
+
+    // Hiding until content is reached
+    revealItems.forEach(function(item) {
+        item.style.opacity = "0";
+        item.style.transform = "translateY(40px)";
+        item.style.filter = "blur(3px";
+        item.style.transition = `opacity 1s ease, transform 1s ease, filter 1s ease`;
+        item.style.transitionDelay= `$[index * 0.08}s`
+    });
+
+    //When entered, apply styles etc.
+    const observer = new IntersectionObserver(function(enters) {
+
+        enters.forEach(function(entry) {
+
+            if (entry.isIntersecting) {
+                entry.target.style.opacity = "1";
+                entry.target.style.transform = "translateY(0)";
+            }
+        });
+    }, {
+        //how much needs to be visible before enter trigger
+        threshold: 0.15
+    });
+
+    revealItems.forEach(function(item) {
+        observer.observe(item);
+    });
 }
